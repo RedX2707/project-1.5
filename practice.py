753cfd3c51c332ec9c5ca7d258b109edb5755987
Release Developments (pty) ltd <139247522+Release-Developments@users.noreply.github.com>
2025-06-13 22:05:41 +0200

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,6 +1,12 @@
 # variables
-
 a = 5 
 b = 7
 c = a + b
-print(c)
+# print(c)
+
+#list
+list_a = [5, 6 , 2, 1]   # This is a list 
+
+
+# Dictionary
+dict_a = {"option_1": 45, "option_2": "this is option 2's value"}  # this is a dictionary (key:value array in some other languages)
